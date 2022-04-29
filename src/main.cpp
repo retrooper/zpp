8818030d83c291632dcaaf6cba3309c0f9a9b6fe
retrooper <retrooperdev@gmail.com>
2022-04-29 19:59:06 +0000

    Comments are being excluded successfully.; Time to tokenize the source code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+#include "SourcePreProcessor.h"
+using namespace zpp;
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    SourcePreProcessor preProcessor;
+    std::ifstream sourceFile("tests/main.zpp");
+    std::string sourceCode = preProcessor.process(sourceFile);
+    std::cout << "Source code: " << sourceCode << std::endl;
     return 0;
 }
